43902ef9b248ef174996c86962d95e919de97dd9
unknown <nowrinx575@gmail.com>
2021-06-06 13:26:28 -0400

    All the methods for product and invoice are added

--- src/invoice/invoice.service.ts
+++ src/invoice/invoice.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { INVOICEModal } from './../modal/invoice.modal';
+import { EntityManager, getManager } from 'typeorm';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class InvoiceService {}
+export class InvoiceService {
+    public tableName = "INVOICE";
+    constructor(private manager: EntityManager) {
+        this.manager = getManager();
+    }
+    public async getAllInvoices(): Promise<INVOICEModal> {
+        let sql = "select * from " + this.tableName + ";"
+        console.log(sql);
+        return await this.manager.query(sql);
+    }
+    public async getAllInvoicesByUserId(data: Number): Promise<INVOICEModal> {
+        try {
+            let sql = "select * from " + this.tableName + " where userID= " + data +  ";"
+            console.log(sql);
+            let result = await this.manager.query(sql);
+            return result;
+        }
+        catch (er) {
+            throw new HttpException("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+    public async insert(data: INVOICEModal): Promise<any> {
+        try {
+            let sql = "INSERT INTO " + this.tableName + "(userID,productID,quantity,totalAmount,invoicedate,status,deliveredDate) VALUES(" + data.userID + "," + data.productID + "," + data.quantity + "," + data.totalAmount + ",'" + data.invoicedate + "','" + data.status + "','" + data.deliveredDate + "')";
+            console.log(sql);
+            let result = await this.manager.query(sql);
+            return result;
+        }
+        catch (er) {
+            console.log(er);
+            throw new HttpException("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+  }
+}
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { PRODUCTModal } from './../modal/product.modal';
+import { Body, Controller, Get, Req,Param } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    constructor(private productService: ProductService) {
+
+    }
+    @Get('getAllProducts')
+    public async getAllProduct(): Promise<PRODUCTModal> {
+        return await this.productService.getAllProducts();
+    }
+    @Get('getAllProductsByProductId/:ProductID')
+    public async getAllProductsByProductId(@Param('ProductID') ProductID ): Promise<PRODUCTModal> {
+        return await this.productService.getAllProductsByProductId(ProductID);
+    }
+}
