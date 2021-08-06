64c39afd115aa2da8ad5536ddc45ca83bbe7193b
jaishilbhavsar <jaishilbhavsar@yahoo.in>
2021-08-06 18:56:31 -0400

    invoices added

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,5 +1,6 @@
+import { INVOICEModal } from './../modal/invoice.modal';
 import { PRODUCTModal } from './../modal/product.modal';
-import { Body, Controller, Get, Req,Param } from '@nestjs/common';
+import { Body, Controller, Get, Req, Param, Post, Delete } from '@nestjs/common';
 import { ProductService } from './product.service';
 
 @Controller('product')
@@ -12,7 +13,19 @@ export class ProductController {
         return await this.productService.getAllProducts();
     }
     @Get('getAllProductsByProductId/:ProductID')
-    public async getAllProductsByProductId(@Param('ProductID') ProductID ): Promise<PRODUCTModal> {
+    public async getAllProductsByProductId(@Param('ProductID') ProductID): Promise<PRODUCTModal> {
         return await this.productService.getAllProductsByProductId(ProductID);
+    }
+    @Get('getAllInvoicesByUserID/:invoiceID')
+    public async getAllInvoicesByUserID(@Param('invoiceID') invoiceID): Promise<PRODUCTModal> {
+        return await this.productService.getAllInvoicesByUserID(invoiceID);
+    }
+    @Post('placeOrder')
+    public async editAppointment(@Req() req, @Body() data: INVOICEModal): Promise<any> {
+        return await this.productService.PlaceOrder(data);
+    }
+    @Delete('deleteOrder/:id')
+    public async deleteAppointment(@Req() req, @Param('id') id: Number): Promise<any> {
+        return await this.productService.deleteOrder(id);
     }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,3 +1,4 @@
+import { INVOICEModal } from './../modal/invoice.modal';
 import { PRODUCTModal } from './../modal/product.modal';
 import { EntityManager, getManager } from 'typeorm';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
@@ -15,7 +16,7 @@ export class ProductService {
     }
     public async getAllProductsByProductId(data: Number): Promise<PRODUCTModal> {
         try {
-            let sql = "select * from " + this.tableName + " where productID= " + data + ";"
+            let sql = "SELECT * FROM " + this.tableName + " p JOIN CATEGORY c ON p.categoryID=c.categoryID where p.productID=" + data + "; ";
             console.log(sql);
             let result = await this.manager.query(sql);
             return result;
@@ -23,5 +24,29 @@ export class ProductService {
         catch (er) {
             throw new HttpException("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR)
         }
+    }
+    public async PlaceOrder(data: INVOICEModal): Promise<PRODUCTModal> {
+        try {
+            let product = await this.getAllProductsByProductId(data.productID);
+            let amount = Number(product[0].price) * Number(data.quantity);
+            let date = new Date().toISOString().split("T")[0];
+            let sql = "INSERT INTO INVOICE (userID,productID,addressID,quantity,totalAmount,invoicedate,status) VALUES(" + data.userID + "," + data.productID + "," + data.addressID + "," + data.quantity + "," + amount + ",'" + date + "','pending')";
+            console.log(sql);
+            let result = await this.manager.query(sql);
+            return result;
+        }
+        catch (er) {
+            throw new HttpException("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+    public async getAllInvoicesByUserID(id: Number): Promise<any> {
+        let sql = "SELECT * FROM INVOICE i JOIN PRODUCT p ON i.productID=p.productID JOIN ADDRESS a ON i.addressID=a.addressID JOIN CATEGORY c ON p.categoryID=c.categoryID WHERE i.userID=" + id + ";"
+        console.log(sql);
+        return await this.manager.query(sql);
+    }
+    public async deleteOrder(id: Number): Promise<any> {
+        let sql = "DELETE FROM INVOICE where invoiceID=" + id + ";"
+        console.log(sql);
+        return await this.manager.query(sql);
     }
 }
